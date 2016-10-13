2722e8eaedce096726808eab159b55372cc335df
Amir Sajjad Poursoltani <sajjadpoores@yahoo.com>
2016-10-13 21:43:18 +0330

    Create 1025 - Fashion Shows.cpp
    http://www.spoj.com/problems/FASHION/

---
+++ 1025 - Fashion Shows.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+	
+	int t; cin >> t;
+	while (t--){
+		vector<int> men, women;
+		int n; cin >> n;
+		for (int i = 0; i < n; i++){
+			int t; cin >> t;
+			men.push_back(t);
+		}
+		for (int i = 0; i < n; i++){
+			int t; cin >> t;
+			women.push_back(t);
+		}
+		sort(men.begin(), men.end());
+		sort(women.begin(), women.end());
+
+		int sum = 0;
+		for (int i = 0; i < n; i++)
+			sum += men[i] * women[i];
+		cout << sum << endl;
+	}
+	return 0;
+}
